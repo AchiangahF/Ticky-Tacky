12c39511658a3ef912e303425568cab512cc7089
Joel Fombin <joelfombin@gmail.com>
2026-02-24 17:50:05 +0100

    added a login button for users with account

--- platform/src/components/Hero.jsx
+++ platform/src/components/Hero.jsx
@@ -17,6 +17,7 @@ function Hero() {
                     <p className="lead text-secondary">It’s only nine squares… but one mistake can cost you everything..</p>
 
                     <button type="button" className="btn btn-outline-success mt-3">Get Started !</button>
+                     <button type="button" className="btn btn-warning mt-3 mx-3 text-light">Login...</button>
                 </div>
             </div>
 
